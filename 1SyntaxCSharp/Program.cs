e33fdf68d0c701aa83d5396b091354b2c0ded7e2
Nico Crapanzano <nico.c15@gso.schule.koeln>
2022-09-01 09:09:34 +0200

    TEST Änderung eingefügt

--- 1SyntaxCSharp/Program.cs
+++ 1SyntaxCSharp/Program.cs
@@ -1,6 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Wanda is the best");
 Console.WriteLine("I love Marvel");
+Console.WriteLine("Peter Parker is the best Spiderman!");
 //TEST
 
 
