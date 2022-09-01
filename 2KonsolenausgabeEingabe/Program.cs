c4c370d0b924ea8ec4cc79a8cb1df8aa0150ee96
Nico Crapanzano <nico.c15@gso.schule.koeln>
2022-09-01 11:02:19 +0200

    KONSOLENAUSGABE/VARIABLEN

---
+++ 2KonsolenausgabeEingabe/Program.cs
@@ -0,0 +1,20 @@
+﻿//A1
+Console.WriteLine( ".-.");
+Console.WriteLine("(o o)");
+Console.WriteLine("| O |");
+Console.WriteLine("|   |");
+Console.WriteLine("'~~~'");
+
+//A2
+string Vorname ="Nico";
+string Nachname ="Crapanzano";
+string Alter = "17";
+Console.WriteLine(Vorname);
+Console.WriteLine(Nachname);
+Console.WriteLine(Alter);
+
+
+//A3
+
+
+
